1c58e1ecd579a4a91c94ae09635edea1b87363ad
Alex J Garcia <alexshuu@gmail.com>
2025-01-09 16:55:31 -0500

    Allow successful backend calls on iPhone

--- src/services/prompts.js
+++ src/services/prompts.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseUrl = process.env.NODE_ENV === 'mobile' ? 
+  import.meta.env.VITE_BACKEND_URL :
+  'http://localhost:3001/prompts';
+
 const getAll = () => {
-  const promise = axios.get('http://localhost:3001/prompts');
+  const promise = axios.get(baseUrl);
   return promise.then((response) => response.data);
 }
 
